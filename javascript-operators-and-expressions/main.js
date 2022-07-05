e19878d17606322795cd89346125d4fe0d760c04
Tarra Ghaffari <tarrag2525@gmail.com>
2022-07-05 19:31:23 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 5;
+var height = 9;
+var area = width * height;
+console.log('Area:', area);
+console.log('Area data type:', typeof (area));
+
+var bill = 198;
+var payment = 323;
+var change = payment - bill;
+console.log('Change:', change);
+console.log('Change data type:', typeof (change));
+
+var quizzes = 93;
+var midterm = 88;
+var final = 95;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Grade:', grade);
+console.log('Grade data type:', typeof (grade));
+
+var firstName = 'Tarra';
+var lastName = 'Ghaffari';
+var fullName = firstName + ' ' + lastName;
+console.log('Full Name:', fullName);
+console.log('Full Name data type:', typeof (fullName));
+
+var pH = 4;
+var isAcidic = pH < 7;
+console.log('Is Acidic:', isAcidic);
+console.log('Is Acidic data type:', typeof (isAcidic));
+
+var headCount = 9283;
+var isSparta = headCount === 300;
+console.log('Is Sparta:', isSparta);
+console.log('Is Sparta data tyoe:', typeof (isSparta));
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+console.log('Motto:', motto);
+console.log('Motto data type:', typeof (motto));
