a7af9ef41172514a09b371fc8e5f7502e6a7097d
Tarra Ghaffari <tarrag2525@gmail.com>
2022-07-05 17:31:27 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName = 'Tarra Ghaffari';
+var isCool = false;
+var totalPets = 0;
+
+console.log('Full Name:', fullName);
+console.log('Is cool:', isCool);
+console.log('Total Pets:', totalPets);
+console.log('Full name type:', typeof (fullName));
+console.log('Is cool type:', typeof (isCool));
+console.log('Total pets type:', typeof (totalPets));
+
+var unknown = null;
+console.log('Unknown:', unknown);
+console.log('Unknown type:', typeof (unknown));
+
+var never;
+console.log('Never:', never);
+console.log('Never type:', typeof (never));
