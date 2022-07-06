b04cb490467bab878550eca60fb7923af1902e08
Tarra Ghaffari <tarrag2525@gmail.com>
2022-07-06 20:18:27 +0000

    Revised javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -7,9 +7,7 @@ var orderHistory = [
       orderNumber: '114-3941689-8772332'
     },
     deliveryDate: 'August 8, 2020',
-    product: [
-      { book: { title: 'Javascript for Impatient programmers', author: 'Rauschmayer, Dr. Axel', returnWindow: 'Sep 7, 2020' } },
-      { other: { title: null, returnWindow: null } }]
+    product: [{ book: { title: 'Javascript for Impatient programmers', author: 'Rauschmayer, Dr. Axel', returnWindow: 'Sep 7, 2020' } }]
   },
 
   {
@@ -20,9 +18,7 @@ var orderHistory = [
       orderNumber: '113-9984268-1280257'
     },
     deliveryDate: 'Jul 20, 2020',
-    product: [
-      { book: { title: 'The Timeless Way of Building', author: 'Alexander,Christopher', returnWindow: 'Aug 19, 2020' } },
-      { other: { title: null, returnWindow: null } }]
+    product: [{ book: { title: 'The Timeless Way of Building', author: 'Alexander,Christopher', returnWindow: 'Aug 19, 2020' } }]
   },
 
   {
@@ -33,9 +29,7 @@ var orderHistory = [
       orderNumber: '114-287557-95059409'
     },
     deliveryDate: 'Jul 7, 2020',
-    product: [
-      { book: { title: null, author: null, returnWindow: null } },
-      { other: { title: 'Gamecube Controller Adapter', returnWindow: 'Aug 5, 2020' } }]
+    product: [{ other: { title: 'Gamecube Controller Adapter', returnWindow: 'Aug 5, 2020' } }]
 
   },
 
@@ -48,7 +42,8 @@ var orderHistory = [
     },
     deliveryDate: 'Jul 5, 2020',
     product: [
-      { book: { title: 'The Art of Sql', author: 'Faroult, Stephane', returnWindow: 'Aug 4, 2020' } },
-      { other: { title: 'Gamecube Controller- Super Smash Bros. Edition', returnWindow: 'Aug 4, 2020' } }]
+      { other: { title: 'Gamecube Controller- Super Smash Bros. Edition', returnWindow: 'Aug 4, 2020' } },
+      { book: { title: 'The Art of Sql', author: 'Faroult, Stephane', returnWindow: 'Aug 4, 2020' } }
+    ]
   }
 ];
