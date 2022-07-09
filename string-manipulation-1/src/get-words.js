07dca055d5cbbd46c9e8aa7433ee2b2e11d36b54
Tarra Ghaffari <tarrag2525@gmail.com>
2022-07-09 00:12:48 +0000

    Updated string-manipulation-1 exercise.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -3,7 +3,7 @@
 // Check if input is a string with characters
 // If no characters, return an empty array
 // Otherwise, split the string using a space identifier and assign the new array to a variable
-// return the variable holding the array from the function
+//    return the variable holding the array from the function
 
 function getWords(string) {
   if (string === '') {
