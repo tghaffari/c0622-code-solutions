e538b4fde87b6326ad6ed4304c8ad2ca7bc4ab50
Tarra Ghaffari <tarrag2525@gmail.com>
2022-07-11 17:38:37 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var heading = document.querySelector('h1');
+console.log('h1:', heading);
+console.dir(heading);
+
+var explanation_ = document.querySelector('#explanation');
+console.log('#explanation:', explanation_);
+console.dir(explanation_);
+
+var hint_ = document.querySelector('.hint');
+console.log('.hint:', hint_);
+console.dir(hint_);
+
+var paragraph = document.querySelectorAll('p');
+console.log('P:', paragraph);
+
+var exampleLink = document.querySelectorAll('.example-link');
+console.log('Example link:', exampleLink);
