6c1971fc9bb1e6c579147681c47e8b3608432563
Tarra Ghaffari <tarrag2525@gmail.com>
2022-07-13 19:56:11 +0000

    Updated javascript-typing-tutor exercise.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,7 +1,14 @@
+// Query all the span elements from the HTML document
+// Set up a variable for the index number
+// Start the first span element in the default stat
+// Declare a function
+// If the user's input matches the letter:
+//     change formatting to correct
+//     Move on to the next letter and change styling to default
+// Otherwise, change styling to wrong. (they won't be able to move on until they
+// push the correct letter)
 
 var $span = document.querySelectorAll('span');
-// var string = 'grumpy wizards make toxic brew';
-// make variable for user input
 var i = 0;
 $span[i].className = 'default';
 function typingTutor(event) {
