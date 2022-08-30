8065572d4a132ad4f6dc4b899f906f1f7dad01d7
Tarra Ghaffari <tarrag2525@gmail.com>
2022-08-30 23:01:51 +0000

    Completed react-class-components

---
+++ react-class-components/install.sh
@@ -0,0 +1,13 @@
+npm init -y
+npm install react
+npm install react-dom
+npm install webpack --save-dev
+npm install webpack-cli --save-dev
+npm install babel-loader --save-dev
+npm install @babel/core --save-dev
+npm install @babel/plugin-transform-react-jsx --save-dev
+touch webpack.config.js
+mkdir src
+mkdir dist
+touch src/index.jsx
+touch dist/index.html
