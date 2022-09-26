551721ec13c46af8f4cbf794b780797d83ff3389
Tarra Ghaffari <tarrag2525@gmail.com>
2022-09-26 18:13:23 +0000

    Completed big-o-notation exercise.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -41,4 +41,4 @@ function uniqueQuadratic(words) {
     }
   }
   return unique;                    // 1 * 1 = O(1)
-} // Big O Notation for uniqueQuadratic: O(?)
+} // Big O Notation for uniqueQuadratic: O(n^2)
