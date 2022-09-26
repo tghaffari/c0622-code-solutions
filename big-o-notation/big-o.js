2fdd99c2b8a279d53c4f67835c14ceded623261d
Tarra Ghaffari <tarrag2525@gmail.com>
2022-09-26 21:36:22 +0000

    Updated  big-o-notation exercise.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -9,7 +9,7 @@ function uniqueLinear(words) {
     i++                             // 2 * n = O(2n) ~= O(n)
   ) {
     const word = words[i];          // 2 * n = O(2n) ~= O(n)
-    if (!seen[word]) {              // 2 * n = O(2n) ~= O(n)
+    if (!seen[word]) {              // 3 * n = O(3n) ~= O(n)
       seen[word] = true;            // 2 * n = O(2n) ~= O(n)
       unique[unique.length] = word; // 3 * n = O(3n) ~= O(n)
     }
@@ -21,18 +21,18 @@ function uniqueQuadratic(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n) ~= O(n)
+    i < words.length;               // 3 * n = O(2n) ~= O(n)
     i++                             // 2 * n = O(2n) ~= O(n)
   ) {
     const word = words[i];          // 2 * n = O(2n) ~= O(n)
     let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n)
-      c < i;                        // 1 * n * n = O(n^2)
+      c < i;                        // 2 * n * n = O(n^2)
       c++                           // 2 * n * n = O(2n^2) ~= O(n^2)
     ) {
       const comparing = words[c];   // 2 * n * n = O(2n^2) ~= O(n^2)
-      if (comparing === word) {     // 1 * n * n = O(n^2)
+      if (comparing === word) {     // 2 * n * n = O(n^2)
         isUnique = false;           // 1 * n * n = O(n^2)
       }
     }
